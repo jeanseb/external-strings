e96cde806431dc67d011738b812f3a30d612b630
Jean-Sébastien Calvier <jean-sebastien.calvier@blablacar.com>
2019-02-26 10:40:49 +0100

    Initial commit

---
+++ app/src/main/java/com/blablacar/poc/externalstrings/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.blablacar.poc.externalstrings
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
